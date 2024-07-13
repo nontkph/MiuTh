1f7dfa1122050bc9af0b699b6e2df056451c9690
MIU <101027146+nontkph@users.noreply.github.com>
2024-07-13 21:49:01 +0700

    Add files via upload

---
+++ object/ui - interface.js
@@ -0,0 +1,18 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+const data = [
+    {
+        Name: "Data 1",
+        Picture: "",
+        Description: "this is description.",
+        Link: "",
+    },
+    {
+        Name: "Data 2",
+        Picture: "",
+        Description: "this is description.",
+        Link: "",
+    }
+];
+
+// สำหรับใช้งานที่อื่นๆ ใน JavaScript
+export default data;
